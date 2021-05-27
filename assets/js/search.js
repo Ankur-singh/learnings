d33f36bd68816eb2c6b0c77ec6da78a5cde37b6d
Ankur-singh <Ankur-singh@users.noreply.github.com>
2021-05-27 14:02:23 +0000

    deploy: 0b246e8d108c9b0beaa8194e28fcdaf0af75a725

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/Learnings/assets/js/search-data.json', true);
+    request.open('GET', '/learnings/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
